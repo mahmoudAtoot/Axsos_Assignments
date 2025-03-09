b57cc56f39cfe9c5418a796cd2760ef6ea68f249
Mahmoud Atoot <mahmoudatoot.99@gmail.com>
2025-03-09 14:29:57 +0200

    initial commit

---
+++ web fun/JS/likes/likes.js
@@ -0,0 +1,17 @@
+var x = document.getElementById('likeCount1')
+function addLike(){
+    console.log(x)
+    x.innerText ++
+}
+var y = document.getElementById('likeCount2')
+function addLike2(){
+    console.log(x)
+    y.innerText ++
+}
+
+var z = document.getElementById('likeCount3')
+function addLike3(){
+    console.log(x)
+    z.innerText ++
+}
+
