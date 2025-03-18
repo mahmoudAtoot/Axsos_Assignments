7875f5de7d5e7f59aa92064eeac75f43a5bf5419
Mahmoud Atoot <mahmoudatoot.99@gmail.com>
2025-03-18 14:30:26 +0200

    initial commit

--- web fun/JS/Calculator/calculator.js
+++ web fun/JS/Calculator/calculator.js
@@ -1,15 +1,25 @@
 var currentValue = "0";
 function updateDisplay() {
-document.querySelector("#display").innerText=currentValue;
+    document.querySelector("#display").innerText = currentValue;
 
 }
 function press(num) {
-    if(document.querySelector("#display").innerText==0){
-        currentValue=num;
-    }else{
-        currentValue+=num;
 
+    if (document.querySelector("#display").innerText == 0) {
+        currentValue = num;
+    } else {
+        currentValue += String(num);
     }
     updateDisplay()
+}
+
+function setOP(){
+    if(document.querySelector("#display").innerText !== 0){
+            
+    }
+
+    
 
 }
+
+
--- web fun/JS/Dojo Weather/DojoWeather.js
+++ web fun/JS/Dojo Weather/DojoWeather.js
@@ -15,8 +15,8 @@ function dismissMessage() {
 
 function convertTemperature() {
     var unit = document.getElementById("temp").value;
-    var highTemps = document.querySelectorAll(".temp-high");
-    console.log(highTemps)
+    // var highTemps = document.querySelectorAll(".temp-high");
+    // console.log(highTemps)
     var lowTemps = document.querySelectorAll(".temp-low");
     var highTemps = document.querySelectorAll(".temp-high");
     for (var i = 0; i < highTemps.length; i++) {
@@ -25,15 +25,18 @@ function convertTemperature() {
 
             var max = (highTemps[i].innerText - 32) * 5 / 9;
             highTemps[i].innerText = max;
-
+            var min = (lowTemps[i].innerText - 32) * 9 / 5
+            lowTemps[i].innerText = min;
 
         }
         var lowTemps = document.querySelectorAll(".temp-low");
         for (var i = 0; i < lowTemps.length; i++) {
             if (unit == "Fahrenheit") {
                 console.log(lowTemps[i].innerText);
-                var min = (lowTemps[i].innerText * 9 / 5) + 32
+                var min = (lowTemps[i].innerText * 9 / 5) + 32;
                 lowTemps[i].innerText = min;
+                var max = (highTemps[i].innerText * 5 / 9) +32;
+                highTemps[i].innerText = max;
             }
         }
 
