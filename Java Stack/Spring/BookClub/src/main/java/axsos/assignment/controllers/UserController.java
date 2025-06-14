5e66116ef7e3428b6e10f0b68b1a2ca82b2b575b
Mahmoud Atoot <mahmoudatoot.99@gmail.com>
2025-06-14 15:54:20 +0300

    java

---
+++ Java Stack/Spring/BookClub/src/main/java/axsos/assignment/controllers/UserController.java
@@ -0,0 +1,65 @@
+package axsos.assignment.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import axsos.assignment.models.LoginUser;
+import axsos.assignment.models.User;
+import axsos.assignment.services.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+
+@Controller
+public class UserController {
+	@Autowired
+	UserService userService;
+
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("newUser", new User());
+		model.addAttribute("newLogin", new LoginUser());
+		return "index";
+	}
+
+	@PostMapping("/register")
+	public String register(@Valid @ModelAttribute("newUser") User newUser, BindingResult result, Model model,
+			HttpSession session) {
+		User registerUser = userService.register(newUser, result);
+
+		if (result.hasErrors()) {
+			model.addAttribute("newLogin", new LoginUser()); // make sure login form is also available
+			return "index"; // keep submitted user data with validation errors
+		}
+
+		session.setAttribute("userId", registerUser.getId());
+		return "redirect:/books";
+	}
+
+	@PostMapping("/login")
+	public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, BindingResult result, Model model,
+			HttpSession session) {
+		User user = userService.login(newLogin, result);
+
+		if (result.hasErrors()) {
+			model.addAttribute("newUser", new User()); // registration form still needs to exist
+			return "index"; // keep login form input & errors
+		}
+
+		session.setAttribute("userId", user.getId());
+		return "redirect:/books";
+	}
+	
+	
+	
+	@PostMapping("/logout")
+	public String logout(HttpSession session) {
+	    session.invalidate();
+	    return "redirect:/";
+	}
+
+}
---
+++ Java Stack/Spring/BookClub/src/main/java/axsos/assignment/repositories/BookRepository.java
@@ -0,0 +1,16 @@
+package axsos.assignment.repositories;
+
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import axsos.assignment.models.Book;
+
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long> {
+    List<Book>findAll(); // Useful for listing all books
+
+}
